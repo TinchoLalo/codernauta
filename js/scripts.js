87a8f4a50506db2ffafaf49b5e0ebd7dd707e1b8
tincholalo <martinuriellaloggia@gmail.com>
2021-04-30 21:05:01 -0300

    preloader

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,7 @@
 (function($) {
     "use strict"; 
 	
-	/* Preloader */
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 500);
-		}
-		hidePreloader();
-	});
+	
 
 	
 	/* Navbar Scripts */
